5792b6d932fab794b2ce6cb5db7d60dc9887a502
zishu34 <w4oxyhun@gmail.com>
2025-07-15 09:58:50 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+window.addEventListener('scroll', () => {
+  document.querySelectorAll('.fade-in').forEach(el => {
+    const rect = el.getBoundingClientRect();
+    if (rect.top < window.innerHeight - 100) {
+      el.style.opacity = 1;
+      el.style.transform = 'translateY(0)';
+    }
+  });
+});
